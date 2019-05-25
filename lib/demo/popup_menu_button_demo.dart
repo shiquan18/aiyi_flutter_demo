abc91b22cc2c211f637d7c8dc9190e0519e0085a
aiyi <609790701@qq.com>
2019-05-25 19:39:53 +0800

    弹出菜单button

--- lib/demo/popup_menu_button_demo.dart
+++ lib/demo/popup_menu_button_demo.dart
@@ -6,6 +6,8 @@ class PopupMenuButtonDemo extends StatefulWidget {
 }
 
 class _PopupMenuButtonDemoState extends State<PopupMenuButtonDemo> {
+  String _currentMenuItem = 'Home';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +22,31 @@ class _PopupMenuButtonDemoState extends State<PopupMenuButtonDemo> {
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[],
+                children: <Widget>[
+                  Text(_currentMenuItem),
+                  PopupMenuButton(
+                    onSelected: (value) {
+                      print(value);
+                      setState(() {
+                        _currentMenuItem = value;
+                      });
+                    },
+                    itemBuilder: (BuildContext context) => [
+                          PopupMenuItem(
+                            value: 'Home',
+                            child: Text('Home'),
+                          ),
+                          PopupMenuItem(
+                            value: 'Discover',
+                            child: Text('Discover'),
+                          ),
+                          PopupMenuItem(
+                            value: 'Community',
+                            child: Text('Community'),
+                          ),
+                        ],
+                  ),
+                ],
               ),
             ],
           ),
