642ee792832e3b60491d850bd02bd6bde5d4f9d6
aiyi <609790701@qq.com>
2019-05-27 13:58:52 +0800

    创建使用

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -8,54 +8,66 @@ class StateManagementDemo extends StatefulWidget {
 class _StateManagementDemoState extends State<StateManagementDemo> {
   int _count = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0.0,
-      ),
-      body: CounterWrapper(_count, _increaseCount),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add), onPressed: _increaseCount),
-    );
-  }
-
   void _increaseCount() {
     setState(() {
       _count += 1;
     });
     print(_count);
   }
-}
-
-class CounterWrapper extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  CounterWrapper(this.count, this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
+    return CounterProvider(
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterWrapper(),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: _increaseCount),
+      ),
+    );
+  }
+}
+
+class CounterWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Center(
-      child: Counter(count, increaseCount),
+      child: Counter(),
     );
   }
 }
 
 class Counter extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  Counter(this.count, this.increaseCount);
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ActionChip(
-        label: Text('$count'),
-        onPressed: increaseCount,
-      ),
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount =
+        CounterProvider.of(context).increaseCount;
+    return ActionChip(
+      label: Text('$count'),
+      onPressed: increaseCount,
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({this.count, this.increaseCount, this.child})
+      : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
   }
 }
