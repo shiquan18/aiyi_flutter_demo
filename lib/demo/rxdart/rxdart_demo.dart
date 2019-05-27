ddbc9fef7473bc333efe9a2cb5771d1466460e38
aiyi <609790701@qq.com>
2019-05-27 15:25:09 +0800

    rxdart map

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -28,7 +28,8 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     super.initState();
     _textFieldSubject = PublishSubject<String>();
 
-    _textFieldSubject.listen((data) => print(data));
+//    _textFieldSubject.listen((data) => print(data));
+    _textFieldSubject.map((item) => 'item:$item').listen((data) => print(data));
 //    Observable<String> _observable =
 //        Observable(Stream.fromIterable(['hello', 'nin hao ']));
 //        Observable.fromFuture(Future.value('hello ~'));
