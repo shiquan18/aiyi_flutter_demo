2e090bfec93066c7aadc9da3992bf8d5dd5b8b25
aiyi <609790701@qq.com>
2019-05-27 11:43:17 +0800

    选择 单选和全选

--- lib/demo/data_table_demo.dart
+++ lib/demo/data_table_demo.dart
@@ -24,6 +24,7 @@ class _DataTableDemoState extends State<DataTableDemo> {
               DataTable(
                 sortColumnIndex: _sortColumnIndex,
                 sortAscending: _sortAscending,
+//                onSelectAll: (bool value) {},
                 columns: [
                   DataColumn(
                     label: Text('Title'),
@@ -50,7 +51,14 @@ class _DataTableDemoState extends State<DataTableDemo> {
                   ),
                 ],
                 rows: posts.map((post) {
-                  return DataRow(cells: [
+                  return DataRow(
+                      selected: post.selected, onSelectChanged: (bool value) {
+                    setState(() {
+                      if (post.selected != value) {
+                        post.selected = value;
+                      }
+                    });
+                  }, cells: [
                     DataCell(Text(post.title)),
                     DataCell(Text(post.author)),
                     DataCell(Image.network(post.imageUrl)),
