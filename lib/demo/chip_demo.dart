49990a6c7c3a5b446c6a184037cce9ef3d2d0c7e
aiyi <609790701@qq.com>
2019-05-27 11:24:12 +0800

    使用列表生成删除功能小碎片

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,51 +6,90 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tags = [
+    'Apple',
+    'Banana',
+    'Lemon',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ChipDemo'),
-          elevation: 0.0,
+      appBar: AppBar(
+        title: Text('ChipDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: <Widget>[
+                Chip(
+                  label: Text('Life'),
+                ),
+                Chip(
+                  label: Text('Sunset'),
+                  backgroundColor: Colors.orange,
+                ),
+                Chip(
+                  label: Text('Wanghao'),
+                  avatar: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Text('皓'),
+                  ),
+                ),
+                Chip(
+                  label: Text('Wanghao'),
+                  avatar: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://resources.ninghao.net/images/wanghao.jpg'),
+                  ),
+                ),
+                Chip(
+                  label: Text('City'),
+                  onDeleted: () {},
+                  deleteIcon: Icon(Icons.delete),
+                  deleteIconColor: Colors.redAccent,
+                  deleteButtonTooltipMessage: 'Remove this tag',
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: () {
+                        setState(() {
+                          _tags.remove(tag);
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ],
         ),
-        body: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: <Widget>[
-                  Chip(
-                    label: Text('Life'),
-                  ),
-                  Chip(
-                    label: Text('Sunset'),
-                    backgroundColor: Colors.orange,
-                  ),
-                  Chip(
-                    label: Text('Wanghao'),
-                    avatar: CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      child: Text('皓'),
-                    ),
-                  ),
-                  Chip(
-                    label: Text('Wanghao'),
-                    avatar: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://resources.ninghao.net/images/wanghao.jpg'),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.grey,
-                    height: 32.0,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: () {
+          setState(() {
+            _tags = [
+              'Apple',
+              'Banana',
+              'Lemon',
+            ];
+          });
+        },
+      ),
+    );
   }
 }
