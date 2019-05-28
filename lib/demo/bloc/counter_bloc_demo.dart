41804c3556771ac92b6e513928b6a71ed7e11f46
aiyi <609790701@qq.com>
2019-05-28 11:52:40 +0800

    bloc stream

--- lib/demo/bloc/counter_bloc_demo.dart
+++ lib/demo/bloc/counter_bloc_demo.dart
@@ -5,15 +5,19 @@ import 'package:flutter/material.dart';
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CounterBloc _counterBloc = CounterProvider
-        .of(context)
-        .bloc;
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return Center(
-      child: ActionChip(
-        label: Text('0'),
-        onPressed: () {
+      child: StreamBuilder(
+        initialData: 0,
+        stream: _counterBloc.count,
+        builder: (context, snapshot) {
+          return ActionChip(
+            label: Text('0 ${snapshot.data}'),
+            onPressed: () {
 //          _counterBloc.log();
-          _counterBloc.counter.add(1);
+              _counterBloc.counter.add(1);
+            },
+          );
         },
       ),
     );
@@ -23,14 +27,12 @@ class CounterHome extends StatelessWidget {
 class CounterActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    CounterBloc _counterBloc = CounterProvider
-        .of(context)
-        .bloc;
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return FloatingActionButton(
       child: Icon(Icons.add),
       onPressed: () {
 //        _counterBloc.log();
-        _counterBloc.counter.add(1);
+        _counterBloc.counter.add(3);
       },
     );
   }
@@ -52,16 +54,22 @@ class CounterProvider extends InheritedWidget {
 }
 
 class CounterBloc {
+  int _count = 0;
   final _counterActionController = StreamController<int>();
 
   StreamSink<int> get counter => _counterActionController.sink;
 
+  final _counterController = StreamController<int>();
+
+  Stream<int> get count => _counterController.stream;
+
   CounterBloc() {
-    _counterActionController.stream.listen(onData)
+    _counterActionController.stream.listen(onData);
   }
 
   void disponse() {
     _counterActionController.close();
+    _counterController.close();
   }
 
   void log() {
@@ -70,5 +78,7 @@ class CounterBloc {
 
   void onData(int data) {
     print('data  $data');
+    _count = data + _count;
+    _counterController.add(_count);
   }
 }
