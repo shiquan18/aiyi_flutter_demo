4e58526b8a1c65893e85144d7e276f899c9ff08b
aiyi <609790701@qq.com>
2019-05-28 14:54:21 +0800

    有变化就重建自己的小部件

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -40,11 +40,14 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
     animation = Tween(begin: 32.0, end: 100.0).animate(curve);
     animationColor =
         ColorTween(begin: Colors.red, end: Colors.red[900]).animate(curve);
-    animationDemoController.addListener(() {
+//    animationDemoController.addListener(() {
 //      print('${animationDemoController.value}');
-      setState(() {});
-    });
+//      setState(() {});
+//    });
 //    animationDemoController.forward();
+    animationDemoController.addStatusListener((AnimationStatus status) {
+      print(status);
+    });
   }
 
   @override
@@ -55,19 +58,38 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: IconButton(
-          icon: Icon(Icons.favorite),
-          iconSize: animation.value,
-          color: animationColor.value,
-          onPressed: () {
-            switch (animationDemoController.status) {
-              case AnimationStatus.completed:
-                animationDemoController.reverse();
-                break;
-              default:
-                animationDemoController.forward();
-            }
-          }),
+      child: AnimatedHeart(
+        animations: [
+          animation,
+          animationColor,
+        ],
+        controller: animationDemoController,
+      ),
     );
+  }
+}
+
+class AnimatedHeart extends AnimatedWidget {
+  final List animations;
+  final AnimationController controller;
+
+  AnimatedHeart({this.animations, this.controller})
+      : super(listenable: controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.favorite),
+        iconSize: animations[0].value,
+        color: animations[1].value,
+        onPressed: () {
+          switch (controller.status) {
+            case AnimationStatus.completed:
+              controller.reverse();
+              break;
+            default:
+              controller.forward();
+          }
+        });
   }
 }
